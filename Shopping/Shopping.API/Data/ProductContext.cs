12a7165df1cfd53c3bd5a6cbaac7366de8b70d2f
Achyutha Krishna <achutha.krrish@gmail.com>
2026-07-22 16:00:37 +0100

    Adding Shopping API project

---
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -0,0 +1,32 @@
+﻿using Shopping.API.Models;
+
+namespace Shopping.API.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product(){
+                Name="Iphone X",
+                Category="Smart Phone",
+                Description="This phone is apple",
+                ImageFile="product-1.png",
+                Price=100.00m
+            },
+            new Product(){
+                Name="Samsung 10",
+                Category="Smart Phone",
+                Description="This phone is Samsung",
+                ImageFile="product-2.png",
+                Price=102.00m
+            },
+            new Product(){
+                Name="LG G7",
+                Category="Home Kitchen",
+                Description="This phone is Biggest",
+                ImageFile="product-6.png",
+                Price=110.00m
+            }
+        };
+    }
+}
